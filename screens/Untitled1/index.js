856915726e5ca4b9aaf2d5ad98fb177975b2ab93
daniel palacios <daniel.palacios@crowdbotics.com>
2022-09-13 13:46:02 -0300

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled1/index.js
@@ -0,0 +1,27 @@
+import { Switch } from "react-native";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled1 = () => {
+  return <View style={_styles.MrjsiwUF}>
+      <Switch style={_styles.sxFMnrSj}></Switch></View>;
+};
+
+export default Untitled1;
+
+const _styles = StyleSheet.create({
+  MrjsiwUF: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  },
+  sxFMnrSj: {
+    left: 176.7766952966368,
+    top: 179.605122421383,
+    position: "absolute",
+    width: 50,
+    height: 25,
+    transform: "rotate(90deg)"
+  }
+});
