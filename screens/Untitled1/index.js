f767b0ef3122b8e339423c317c9e82d57616e828
daniel palacios <daniel.palacios@crowdbotics.com>
2022-09-13 15:03:18 -0300

    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,10 +1,11 @@
+import { TextInput } from "react-native";
 import { Switch } from "react-native";
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
 const Untitled1 = () => {
   return <View style={_styles.MrjsiwUF}>
-      <Switch style={_styles.sxFMnrSj}></Switch></View>;
+      <Switch style={_styles.sxFMnrSj}></Switch><View style={_styles.HWmKDyIh}></View><TextInput style={_styles.xgUaKLuF}></TextInput><Switch style={_styles.KAuGlQMi}></Switch></View>;
 };
 
 export default Untitled1;
@@ -23,5 +24,35 @@ const _styles = StyleSheet.create({
     width: 50,
     height: 25,
     transform: "rotate(90deg)"
+  },
+  HWmKDyIh: {
+    left: 49.29577464788732,
+    top: 38.028169014084504,
+    position: "absolute",
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  uSPDRcrS: {
+    position: "unset"
+  },
+  xgUaKLuF: {
+    left: 112.67605633802816,
+    top: 274.6478873239436,
+    position: "absolute",
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30
+  },
+  KAuGlQMi: {
+    left: 105.63380281690141,
+    top: 394.36619718309856,
+    position: "absolute",
+    width: 50,
+    height: 25,
+    transform: "rotate(45deg)"
   }
 });
